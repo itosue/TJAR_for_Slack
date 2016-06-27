099277d01db5fd0f1052cf2eb3c740b2f9af99c1
itosue <itocchi@jasmine.colored-leaves.net>
2016-06-27 22:13:32 +0900

    First release

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # TJAR_for_Slack
-Throw JSON web hook to Slack 
+TJAR - Throw JSON After Rendering. 
+
+This addon throws JSON webhook to Slack after rendering. You don't need to check whteher rendering is finished or not anymore.
+You can send notification to any device via slack.(I get notifications from Pebble via SlackApp.) 
+
+This addon sends 2 messages.  
+1)render canceled : This will be sent when rendering is canceled (ex.closing window etc...)
+2)render completed : This will be sent end when rendering is completed.
+
+Message customization is now work in progress. I left the codes as comment so use it if you want.
+I wrote this as simple JSON sender so you may tweak this code for other services easily.(ex. Just Changing URL&JSON body and it might adopts IFTTT's Maker etc...)
+
+
+日本語ヘルプ  
+Render終了時にSlackへMessageを投げるスクリプトです。
+一晩かかるレンダリングならすぐに諦めて寝たりできますが、15分や20分等、中途半端なレンダリング時間だと、終わったかどうか気になってチラチラ見ては「まだか〜〜〜〜」ってがっかりするのに懲りたので作ってみました。
+
+もっと汎用的にJSONを投げるアドオンにしたかったのですが、時間が無いのでとりあえずSlack特化でリリースします。
+ちょっとコード読める人なら簡単に改造できると思いますので、色々いじってみてください。（URLとJSONのコードの部分を変えたらIFTTT用とかにすぐ改造できると思います。）私はSlack→SlackのAndroidApp→Notification→Pebbleという感じで、通知を受け取るようにしています。
+
+投げられるメッセージ  
+1)レンダーがキャンセルされた時（Windowが閉じられた時など）  
+2)レンダー（若しくはアニメーション）出力完了時  
+
+メッセージのカスタマイズも実装しようとしたのですが、中途半端になったので切り捨てました。
+カスタマイズしたい人はコードの中にコメントで残してますので、適宜改造して動かして下さい。
+初めてのPythonのコード＆Blenderのライブラリなので、変なとこもあるかもですが、ご容赦下さいm(_ _)m
---
+++ tjar4s.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import bpy
+from bpy.app.handlers import persistent
+from bpy.types import Operator, AddonPreferences
+from bpy.props import StringProperty, IntProperty, BoolProperty
+import json
+import urllib.request
+
+bl_info = {
+    "name": "throw json after rendering for Slack",
+    "author": "itosue",
+    "version": (1, 0),
+    "blender": (2, 77, 0),
+    "location": "Properties > Render",
+    "description": "This addon sends message to Slack after rendering.",
+    "warning": "",
+    "support": "TESTING",
+    "wiki_url": "",
+    "tracker_url": "https://github.com/itosue/TJAR_for_Slack/issues",
+    "category": "Render"
+}
+
+class tjafAddonPreferences(AddonPreferences):
+    bl_idname = __name__
+    
+    URL = StringProperty(
+        name="Webhook URL",
+        default="https://hooks.slack.com/services/XXXX/YYYY/ZZZZZZ",
+        )
+    cn_name = StringProperty(
+        name="Channel",
+        default="#general",
+        )
+
+# May be implement later
+#    message = StringProperty(
+#            name="Message",
+#            default="render finished",
+#        )
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="Please set your WebhookURL and Channel")
+        layout.prop(self, "URL")
+        layout.prop(self, "cn_name")
+#        layout.prop(self, "message") # May be implement later
+
+        
+class ObjectCheckPanel(bpy.types.Panel):
+    bl_idname = "OBJECT_PT_check"
+    bl_label = "Throw JSON After Rendering"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "render"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    bpy.types.Scene.MyBool = BoolProperty(
+        name = "send notification",
+        default = True,
+        description = "Check to send notification.")
+    
+    def draw(self, context):
+        layout = self.layout
+        scn = context.scene
+        layout.prop(scn, 'MyBool')
+        
+        
+def throw_json(scene,message):
+    if scene.MyBool :
+        print("trying to send message \"" + message +"\"...")
+        user_preferences = bpy.context.user_preferences    
+        addon_prefs = user_preferences.addons[__name__].preferences
+        slackURL = addon_prefs.URL
+        payload = {
+            "channel" : addon_prefs.cn_name,
+            #"text" : addon_prefs.message, # May be implement later.
+            "text" : message,
+            "username" : "blender",
+            "icon_emoji" : "ghost"
+        }
+        encoded_data = json.dumps(payload).encode('utf8')
+        req = urllib.request.Request(slackURL,data=encoded_data,headers={'content-type': 'application/json'})
+        response = urllib.request.urlopen(req)
+        print("message sent. response is \"" + response.read().decode('utf8') + "\"")
+        return {'FINISHED'}
+    else:
+        print("sending notification is disabled")
+        return {'FINISHED'}
+
+def render_canceled(scene):
+    throw_json(scene,"render canceled")
+    
+def render_finished(scene):
+    throw_json(scene,"render completed")
+
+def register():
+    bpy.utils.register_class(tjafAddonPreferences)
+    bpy.utils.register_class(ObjectCheckPanel)
+    bpy.app.handlers.render_cancel.append(render_canceled)
+    bpy.app.handlers.render_complete.append(render_finished)
+
+def unregister():
+    bpy.utils.unregister_class(tjafAddonPreferences)
+    bpy.utils.unregister_class(ObjectCheckPanel)
+    bpy.app.handlers.render_cancel.remove(render_canceled)
+    bpy.app.handlers.render_complete.remove(render_finished)
+
+if __name__ == "__main__":
+    register()
